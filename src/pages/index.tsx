66717d30f04a9114acc958cfec5e8925519fb054
jayro <jayro@pop-os.localdomain>
2021-01-26 17:41:27 -0300

    Alterações no projeto para acompanhar o curso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,6 @@ interface IHomeProps {
 }
 
 export default function Home({ recommendedProducts }: IHomeProps) {
-  async function handleSum() {
-    const math = (await import ('@/lib/math')).default;
-    alert(math.sum(3, 5));
-  }
 
   return (
     <div>
@@ -40,7 +36,6 @@ export default function Home({ recommendedProducts }: IHomeProps) {
           })}
         </ul>
       </section>
-      {/* <button onClick={handleSum}>Sum!</button> */}
     </div>
   )
 }
